beca095d0969fe33a2a12639b6d9e4538adbd352
Olivia Park <olivia.park1346@gmail.com>
2020-10-21 07:34:12 +0900

    restructured files

--- features/op_education.js
+++ features/op_education.js
@@ -1,5 +1,8 @@
 const { Botkit } = require('botkit');
 
 module.exports = function(controller) {
-
+  controller.hears('education', 'message', async(bot, message) => {
+    await bot.reply(message, {type: 'typing'});
+    await bot.reply(message, ``);
+  });
 }

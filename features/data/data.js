cfdb1c0045cb2e2e5fffbaab9ce9bec37e2ef72f
Olivia Park <olivia.park1346@gmail.com>
2020-10-22 07:31:07 +0900

    update data.js

---
+++ features/data/data.js
@@ -0,0 +1,291 @@
+module.exports = {
+  jen: {
+    firstName: "Jen",
+    lastName: "Lu",
+    description: "<strong>Jen</strong> is a web developer, who in her free time enjoys trying new restaurants, playing animal crossing, and doodling on her iPad.",
+    jobSearch: true,
+    willRelocate: true,
+    jobHistory: [
+      {
+        companyName: "McCalls Catering / Cafe 5",
+        location: "San Francisco, CA",
+        jobTitle: "Barista / Cashier",
+        jobDescription: "Provided excellent customer service and created appealing daily product displays to increase sales.",
+        startDate: "October 2018",
+        endDate: "November 2019"
+      },
+      {
+        companyName: "UNIQLO",
+        location: "San Francisco, CA",
+        jobTitle: "Sales Associate",
+        jobDescription: "Assisted all customer needs by following UNIQLO service standards and procedures. Consistently received positive feedback from weekly online reviews and was promoted in the first 3 months.",
+        startDate: "March 2017",
+        endDate: "September 2017"
+      },
+      {
+        companyName: "Virginia Tech Department of Forestry",
+        location: "Blacksburg, VA",
+        jobTitle: "Research Assistant",
+        jobDescription: "Assisted PhD students with ongoing research of Poplar tree genetics and functional genomics. Organized an efficient workflow in the lab by properly documenting and maintaining over one thousand genetically modified plants.",
+        startDate: "October 2016",
+        endDate: "February 2017"
+      },
+      {
+        companyName: "Sushi Factory",
+        location: "Blacksburg, VA",
+        jobTitle: "Assistant Manager",
+        jobDescription: "Trained all new employees and maintained all social media platforms",
+        startDate: "June 2016",
+        endDate: "February 2017"
+      },
+      {
+        companyName: "OPass Tea and Crepe",
+        location: "Blacksburg, VA",
+        jobTitle: "Assistant Manager",
+        jobDescription: "Oversaw all daily activies, recorded/filed monthly and quarterly taxes",
+        startDate: "August 2013",
+        endDate: "May 2016"
+      },
+    ],
+    education: [
+      {
+        institutionName: "Virginia Tech",
+        degree: "B.S. Fashion Merchandising and Design",
+        description: "Lorem ipsum dolor sit amet",
+        startDate: "August 2012",
+        endDate: "May 2016"
+      },
+      {
+        institutionName: "CSU East Bay",
+        degree: "PreProfessional Health Academic Program",
+        description: "Lorem ipsum dolor sit amet",
+        startDate: "September 2017",
+        endDate: "May 2019"
+      },
+      {
+        institutionName: "App Academy",
+        degree: "Full Stack Web Development",
+        description: "Lorem ipsum dolor sit amet",
+        startDate: "December 2019",
+        endDate: "April 2020"
+      },
+    ],
+    technologies: [
+      {
+        name: "JavaScript",
+        years: "1"
+      },
+      {
+        name: "React.js",
+        years: "1"
+      },
+      {
+        name: "Node.js",
+        years: "1"
+      },
+      {
+        name: "Express.js",
+        years: "1"
+      },
+      {
+        name: "Ruby",
+        years: "1"
+      },
+      {
+        name: "Ruby on Rails",
+        years: "1"
+      },
+      {
+        name: "HTML",
+        years: "2"
+      },
+      {
+        name: "CSS",
+        years: "2"
+      },
+      {
+        name: "jQuery",
+        years: "1"
+      },
+      {
+        name: "Git",
+        years: "1"
+      },
+      {
+        name: "Postgresql",
+        years: "1"
+      },
+      {
+        name: "AWS",
+        years: "1"
+      },
+      {
+        name: "MongoDB",
+        years: "1"
+      },
+      {
+        name: "SQL",
+        years: "1"
+      },
+      {
+        name: "",
+        years: ""
+      },
+    ],
+    projects: [
+      {
+        name: "Sushi Factory Website",
+        description: "A website created for sushi restaurant looking to increase online presence and provide easy access to menu.",
+        technologies: ["JavaScript", "React.js", "CSS"],
+        url: "https://sushi-factory.herokuapp.com/#/",
+        img: "./images/sushi.png"
+      },
+      {
+        name: "Puppr",
+        description: "A puppy themed clone of photo uploading site, Flickr. Upload your cutest puppy photos, create albums, and more.",
+        technologies: ["Ruby", "Ruby on Rails", "Postgresql", "AWS", "JavaScript", "React.js", "Redux.js", "CSS"],
+        url: "http://puppr-project.herokuapp.com/#/",
+        img: "./images/fsp.png"
+      },
+      {
+        name: "Zi Match",
+        description: "A card matching game to learn simple Chinese words and phrases. Includes a study section with flashcards to initially learn these words/phrases.",
+        technologies: ["JavaScript", "HTML", "CSS"],
+        url: "http://hellojenlu.com/zi-match/",
+        img: "./images/js.png"
+      },
+      {
+        name: "Bored Gamer",
+        description: "A web app built by a team of 3 that helps indecisive board gamers choose what to play. Games can be rated and saved for future reference.",
+        technologies: ["JavaScript", "MongoDB", "Express.js", "React.js", "Node.js", "CSS"],
+        url: "https://bored-gamer.herokuapp.com/",
+        img: "./images/mern.png"
+      },
+    ],
+    contactInfo: {
+      name: "Jennifer Lu",
+      email: "nejul33@gmail.com",
+      portfolio: "http://hellojenlu.com",
+      linkedIn: "https://www.linkedin.com/in/jennifer-l-5b1b83a2/",
+      github: "https://github.com/jenlu33",
+
+    }
+  },
+
+  olivia: {
+    firstName: "Olivia",
+    lastName: "Park",
+    description: "<strong>Olivia</strong> is a biologist-turned-software engineer, who enjoys spending time with her dogs and thinking of new startup ideas.",
+    jobSearch: true,
+    willRelocate: true,
+    jobHistory: [
+      {
+        companyName: "Color Genomics",
+        location: "Burlingame, CA",
+        jobTitle: "Clinical Lab Associate",
+        jobDescription: "As one of the first Clinical Lab Associates (CLA), Olivia was integral in defining the role and providing guidance to other CLAs.",
+        startDate: "January 2017",
+        endDate: "August 2018"
+      },
+      {
+        companyName: "Scribe America",
+        location: "Salem, VA",
+        jobTitle: "Medical Scribe",
+        jobDescription: "Accompanied multiple emergency department physicians during patient consultations and assisted in recording encounters into the electronic medical record.",
+        startDate: "December 2018",
+        endDate: "September 2019"
+      },
+    ],
+    education: [
+      {
+        institutionName: "Virginia Tech",
+        degree: "M.S. Biochemistry",
+        description: "Lorem ipsum dolor sit amet",
+        startDate: "August 2018",
+        endDate: "May 2020"
+      },
+      {
+        institutionName: "University of Illinois at Urbana-Champaign",
+        degree: "B.S. Molecular and Cellular Biology",
+        description: "Lorem ipsum dolor sit amet",
+        startDate: "August 2012",
+        endDate: "May 2016"
+      },
+    ],
+    technologies: [
+      {
+        name: "JavaScript",
+        years: "1"
+      },
+      {
+        name: "React.js",
+        years: "1"
+      },
+      {
+        name: "Node.js",
+        years: "1"
+      },
+      {
+        name: "Express.js",
+        years: "1"
+      },
+      {
+        name: "HTML",
+        years: "2"
+      },
+      {
+        name: "CSS",
+        years: "2"
+      },
+      {
+        name: "jQuery",
+        years: "1"
+      },
+      {
+        name: "Git",
+        years: "1"
+      },
+      {
+        name: "SQL",
+        years: "1"
+      }
+    ],
+    projects: [
+      {
+        name: "Portfolio Site",
+        description: "A portfolio site of all the projects Olivia completed during bootcamp. These projects include a random quote generator, a DOM manipulation project, an interactive form, a game show app, and an API project.",
+        technologies: ["JavaScript", "Node.js", "Express"],
+        url: "www.oliviahpark.com",
+        img: ""
+      },
+      {
+        name: "React Gallery App",
+        description: "A React Gallery app that is similar to Flickr.",
+        technologies: ["JavaScript", "React", "React Router", "Fetch API", "JSX"],
+        url: "",
+        img: ""
+      },
+      {
+        name: "SQL Library Manager",
+        description: "A website that manages a library database. Allowing the user to create, read, update, and delete books.",
+        technologies: ["JavaScript", "Node.js", "Express", "Sequelize"],
+        url: "",
+        img: ""
+      },
+      {
+        name: "REST API Project",
+        description: "Backend portion of a fullstack project for managing courses at an imaginary school.",
+        technologies: ["JavaScript", "Sequelize", "Express.js", "Node.js"],
+        url: "",
+        img: ""
+      },
+    ],
+    contactInfo: {
+      name: "Olivia Park",
+      email: "olivia.park1346@gmail.com",
+      portfolio: "www.oliviahpark.com",
+      linkedIn: "https://www.linkedin.com/in/olivia-park-2b564174/",
+      github: "https://github.com/op1346",
+    }
+  }
+}

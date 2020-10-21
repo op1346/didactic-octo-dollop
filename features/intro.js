0f9d869f51cdd8678d12c0defe42db9888605c30
jenlu33 <nejul33@gmail.com>
2020-10-20 22:52:08 -0400

    Add initial intro chat and jen_contact

--- features/intro.js
+++ features/intro.js
@@ -1,5 +1,29 @@
-const { Botkit } = require('botkit');
+// const { Botkit } = require('botkit');
 
 module.exports = function(controller) {
+  // automatically says hi when user enters
+  controller.on('welcome_back', async(bot, message) => {
+    await bot.reply(message, {type: "typing"});
+
+    setTimeout(async () => {
+      await bot.changeContext(message.reference);
+      await bot.reply(message, "Hi there!");
+      await bot.reply(message, {type: "typing"});
+    }, 1000)
+
+    setTimeout(async () => {
+      await bot.changeContext(message.reference);
+      await bot.reply(message, "My name is JO the OJ Bot and I was created by Jen Lu and Olivia Park");
+      await bot.reply(message, {type: "typing"});
+    }, 2000)
+
+    setTimeout(async () => {
+      await bot.changeContext(message.reference);
+      await bot.reply(message, "Who would you like more information on? You can also learn about both!");
+    }, 3000)
+
+
+
+  })
 
 }

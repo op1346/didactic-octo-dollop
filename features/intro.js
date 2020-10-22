ad9d5522432c9c1d689038b5aa1f2cd221458c00
jenlu33 <nejul33@gmail.com>
2020-10-22 17:36:32 -0400

    Add fun data

--- features/intro.js
+++ features/intro.js
@@ -2,27 +2,35 @@ const data = require("./data/data");
 const {jen, olivia} = data;
 
 module.exports = function(controller) {
-  // automatically says hi when user enters
-  controller.on('welcome_back', async(bot, message) => {
+  // automatically says welcome message when user enters
+  controller.on("hello", async(bot, message) => {
+    await welcomeMessage(bot, message);
+  });
+
+  controller.on("welcome_back", async(bot, message) => {
+    await welcomeMessage(bot, message);
+  });
+
+  const welcomeMessage = async(bot, message) => {
     await bot.reply(message, {type: "typing"});
 
     setTimeout(async () => {
       await bot.changeContext(message.reference);
       await bot.reply(message, "Hi there!");
       await bot.reply(message, {type: "typing"});
-    }, 2000)
+    }, 2500)
 
     setTimeout(async () => {
       await bot.changeContext(message.reference);
       await bot.reply(message, `My name is JO the OJ Bot and I was created by <strong>Jen Lu</strong> and <strong>Olivia Park</strong>`);
       await bot.reply(message, {type: "typing"});
-    }, 2500)
+    }, 3500)
 
     setTimeout(async () => {
       await bot.changeContext(message.reference);
       await bot.reply(message, `<img src="./images/jo1.jpg" alt="jen and olivia" width="200">`);
       await bot.reply(message, {type: "typing"});
-    }, 3000)
+    }, 5000)
 
     let jobText;
     if (jen.jobSearch && olivia.jobSearch) {
@@ -46,7 +54,7 @@ module.exports = function(controller) {
       await bot.changeContext(message.reference);
       await bot.reply(message, `${jobText} ${relocationText}`);
       await bot.reply(message, {type: "typing"});
-    }, 5000)
+    }, 6000)
 
     setTimeout(async () => {
       await bot.changeContext(message.reference);
@@ -67,8 +75,71 @@ module.exports = function(controller) {
           }
         ]
       });
-    }, 7000)
+    }, 8000)
+
+  }
+
+  controller.hears(["hey", "hello", "what's up", "help"], ['message', 'direct_message'], async(bot, message) => {
+    await bot.reply(message, {type: "typing"});
+
+    setTimeout(async () => {
+      await bot.changeContext(message.reference);
+      await bot.reply(message, "Hello! My name is JO");
+      await bot.reply(message, {type: "typing"});
+    }, 1000)
+
+    setTimeout(async () => {
+      await bot.changeContext(message.reference);
+      await bot.reply(message, "I am here to help you learn about my creators, Jen and Olivia");
+      await bot.reply(message, {type: "typing"});
+    }, 3000)
+
+    setTimeout(async () => {
+      await bot.changeContext(message.reference);
+      await bot.reply(message, {
+        text: "You can type your own response in the chatbox or click on the buttons below",
+        quick_replies: [
+          {
+            title: "Learn about Jen",
+            payload: "Jen"
+          },
+          {
+            title: "Learn about Olivia",
+            payload: "Olivia"
+          },
+          {
+            title: "Something Fun!",
+            payload: "Something Fun"
+          },
+        ]
+      });
+    }, 4000)
 
   })
+
+  controller.on("message, direct_message", async(bot, message) => {
+    await bot.reply(message, {type: "typing"});
+    
+    setTimeout(async () => {
+      await bot.changeContext(message.reference);
+      await bot.reply(message, {
+        text: "Sorry, I am still learning and don't understand what you just typed. Please try again or select one of the options below",
+        quick_replies: [
+          {
+            title: "Learn about Jen",
+            payload: "Jen"
+          },
+          {
+            title: "Learn about Olivia",
+            payload: "Olivia"
+          },
+          {
+            title: "Something Fun!",
+            payload: "Something Fun"
+          },
+        ]
+      })
+    }, 1500)
+  })  
 
 }
